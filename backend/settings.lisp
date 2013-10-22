ebd95299c50f2f4e0b3debebbe8b28f55cb29625
kubov <kubov2@gmail.com>
2013-10-22 11:13:43 +0200

    huge change in function organisation for the sake of reusability

--- backend/settings.lisp
+++ backend/settings.lisp
@@ -1,19 +1,23 @@
 (in-package :octopus)
 
+;slots for threads
 (defparameter *server-thread* nil)
-(defparameter *resource-thread* nil)
+(defparameter *channel-manager-resource-thread* nil)
 
+;server options
 (defparameter *port* 7878)
-(defparameter *resource-path* "/api")
-(defparameter *resource-listener-name* "octopus api")
 (defparameter *server-thread-name* "octopus server thread")
-(defparameter *resource-origin-prefixes* '(""))
+
+;channel manager WebSocket listner options
+(defparameter *channel-manager-resource-path* "/channel-manager")
+(defparameter *channel-manager-resource-listener-name* "octopus api")
+(defparameter *channel-manager-resource-origin-prefixes* '(""))
 
 ;log options
 (defparameter *log-sender-name* 'octopus-log)
 (defparameter *log-location* "./octopus.log")
 
-;database
+;database options
 (defparameter *database-host* "localhost")
 (defparameter *database-user* "octopus")
 (defparameter *database-name* "octopus")
--- backend/volatile.lisp
+++ backend/volatile.lisp
@@ -12,3 +12,21 @@
    (creation-time (get-universal-time) :type date)
    (worker nil)))
 
+;channel manager resource
+(def class* channel-manager-resource (ws-resource)
+  ())
+
+(defmethod resource-client-connected ((res channel-manager-resource) client)
+  (log-as info "client connected on channel-manager server from ~s : ~s" (client-host client) (client-port client))
+  t)
+
+(defmethod resource-client-disconnected ((resource channel-manager-resource) client)
+  (log-as :info "client disconnected from resource ~A" resource))
+
+(defmethod resource-received-text ((res channel-manager-resource) client message)
+  (log-as info "got frame ~s... from client ~s" (subseq message 0 10) client)
+  (write-to-client-text client message))
+
+(defmethod resource-received-binary((res channel-manager-resource) client message)
+  (log-as info "got binary frame len: ~s" (length message) client)
+  (write-to-client-binary client message))
