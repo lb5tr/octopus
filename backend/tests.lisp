92dbbb27f2a71d3d89e0180f86947c8ddbce34d0
kubov <kubov2@gmail.com>
2013-11-08 17:23:21 +0100

    added one more simple test.

--- backend/tests.lisp
+++ backend/tests.lisp
@@ -5,8 +5,14 @@
 (fiveam:in-suite basic)
 
 (fiveam:test basic-tests
-  "Login"
+  "Login-should-not-pass"
   (fiveam:is (string=
               (login (make-instance 'user-v :username "test-abc" :password-hash "badpassword") nil)
               "{\"messageType\":\"error\",\"payload\":{\"errorCode\":0,\"errorDescription\":\"NO-SUCH-USER\"}}")
-             "Accepted user with bad password (or something else)"))
+              "Accepted user with bad password (or something else)")
+  "Login-should-pass"
+  (fiveam:is (progn
+               (let ((json (login (make-instance 'user-v
+                                                 :username "test-user"
+                                                 :password-hash (hash-string "abc" :digest *default-digest*)) nil)))
+                 (get-user *server* "test-user" :users-by 'users-by-username-of)))))
